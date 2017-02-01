9d73aa726ce1cf263909462fefc3c6ca7ca7a430
Marco Lotto <marcol91@gmail.com>
2017-02-01 17:41:23 -0300

    Initial basic examples, taking images from web camera and applying filters

---
+++ demo.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+// OpenCV includes
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+
+// OpenCV command line parser functions
+// Keys accecpted by command line parser
+const char* keys =
+{
+"{help h usage ? | | print this message}"
+"{@video | | Video file, if not defined try to use webcamera}"
+};
+
+Mat calculateLightPattern(Mat img)
+{
+	Mat pattern;
+	// Basic and effective way to calculate the light pattern from one image
+	blur(img, pattern, Size(img.cols/3,img.cols/3));
+	return pattern;
+}
+
+Mat removeLight(Mat img, Mat pattern, int method)
+{
+	Mat aux;
+	// if method is normalization
+	if(method==1)
+	{
+		// Require change our image to 32 float for division
+		Mat img32, pattern32;
+		img.convertTo(img32, CV_32F);
+		pattern.convertTo(pattern32, CV_32F);
+		// Divide the image by the pattern
+		aux= 1-(img32/pattern32);
+		// Scale it to convert to 8bit format
+		aux=aux*255;
+		// Convert 8 bits format
+		aux.convertTo(aux, CV_8U);
+	}else{
+		aux= pattern-img;
+	}
+	return aux;
+}
+
+Mat removeNoise(Mat source){
+	Mat img_noise;
+	medianBlur(source, img_noise, 3);
+	return img_noise;
+}
+
+int main( int argc, const char** argv )
+{
+	CommandLineParser parser(argc, argv, keys);
+	parser.about("Chapter 2. v1.0.0");
+	//If requires help show
+	if (parser.has("help"))
+	{
+		parser.printMessage();
+		return 0;
+	}
+	String videoFile= parser.get<String>(0);
+	// Check if params are correctly parsed in his variables
+	if (!parser.check())
+	{
+		parser.printErrors();
+		return 0;
+	}
+	VideoCapture cap; // open the default camera
+	if(videoFile != "")
+		cap.open(videoFile);
+	else
+		cap.open(0);
+	if(!cap.isOpened()) // check if we succeeded
+		return -1;
+	namedWindow("Video",1);
+	for(;;)
+	{
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+
+		// Reducimos el noise de la imagen
+		frame = removeNoise(frame);
+
+		// Removemos el background por aproximacion
+		//frame = removeLight(frame, calculateLightPattern(frame), 1);
+
+		imshow("Video", frame);
+		waitKey(100);
+	}
+	// Release the camera or video cap
+	cap.release();
+	return 0;
+}
